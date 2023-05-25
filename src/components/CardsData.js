115e008af1ed5504cc4ab369bb41fb39210bd4ec
NavneetOrion <navneet.kumar@deeporion.com>
2023-05-25 16:28:04 +0530

    Cart Project in Redux

---
+++ src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Dying Light 2",
+        imgdata: "https://m.media-amazon.com/images/I/71YnOZfQXCS._SX425_.jpg",
+        address: "Dying Light 2",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " order placed from here recently",
+        price: 1499,
+        rating: "4.7",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Watch Dogs 2",
+        imgdata: "https://static-01.daraz.pk/p/b7ac381865d306934693c626de0fc148.jpg_720x720.jpg_.webp",
+        address: "Watch Dogs 2",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1500+ order placed from here recently",
+        price: 1500,
+        rating: "4.6",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Grand Theft auto V",
+        imgdata: "https://image.smythstoys.com/original/desktop/210029.jpg",
+        address: "Grand theft Auto V",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 999 + order placed from here recently",
+        price: 999,
+        rating: "4.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Uncharted The Lost Legacy",
+        imgdata: "https://www.mimovrste.com/i/76267166",
+        address: "Uncharted",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1200 + order placed from here recently",
+        price: 1200,
+        rating: "4.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Watch Dogs Legion",
+        imgdata: "https://m.media-amazon.com/images/I/81zqGrI0WbL._SL1500_.jpg",
+        address: "Watch Dogs Legion",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1999 + order placed from here recently",
+        price: 1999,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Cyberpunk 2077",
+        imgdata: "https://m.media-amazon.com/images/I/81jlVYgx3lL._SL1500_.jpg",
+        address: "Cyberpunk 2077",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2499 + order placed from here recently",
+        price: 2499,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "God of War",
+        imgdata: "https://m.media-amazon.com/images/I/81E5a+Vym-L._SL1500_.jpg",
+        address: "God of War",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "3000 + order placed from here recently",
+        price: 3000,
+        rating: "4.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Dead Island 2",
+        imgdata: "https://game-legends.de/media/fa/ce/b3/1673344402/1112368-dead-island-2-pulp-edition-ps5-usk-2d.webp",
+        address: "Dead Island 2",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 1400,
+        rating: "4.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "The Last of US Part 1",
+        imgdata: "https://m.media-amazon.com/images/I/81paEK4OI4L._SL1500_.jpg",
+        address: "The Last of US Part 1",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2000 + order placed from here recently",
+        price: 2000,
+        rating: "4.1",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
